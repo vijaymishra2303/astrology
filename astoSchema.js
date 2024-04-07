d07eb8d991cfccb6e78a5c2e0e194ead4a2bc456
vijaymishra2303 <vijaymishra2303@gmail.com>
2024-04-07 16:49:26 +0530

    astro

---
+++ astoSchema.js
@@ -0,0 +1,29 @@
+// const mongoose=require("mongoose")
+// const astoSchema=mongoose.Schema({
+//     pid:Number,
+//     pname:String,
+//     pimage:String
+// })
+// module.exports=mongoose.model("product",astoSchema)
+
+
+
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    pid: Number,
+    pname: String,
+    pdesc:String,
+    pimage: String
+});
+
+const userSchema = mongoose.Schema({
+    uid: Number,
+    username: String,
+    email: String
+});
+
+const Product = mongoose.model("Product", productSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = { Product, User };
---
+++ index.js
@@ -0,0 +1,83 @@
+
+require("./mongoose")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const multer = require("multer");
+const { Product, User } = require("./astoSchema"); // Importing models
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+// Multer configuration for file upload
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage }).single("pimage");
+
+// POST method for creating a new product
+app.post("/product", async (req, res) => {
+    upload(req, res, async (err) => {
+        if (err) {
+            console.log(err);
+           
+        } else {
+                const newProduct = new  Product({
+                    pid: req.body.pid,
+                    pname: req.body.pname,
+                    pdesc:req.body.pdesc,
+                    pimage: "http://localhost:4000/uploads/" + req.file.filename
+                });
+                 newProduct.save();
+                res.send("File Uploaded")
+            
+        }
+    });
+});
+
+
+app.get("/product",async (req,resp)=>{
+        const products=await Product.find()
+        resp.send(products)
+    })
+
+
+
+
+//user collection
+app.post("/user", async (req, res) => {
+    upload(req, res, async (err) => {
+        if (err) {
+            console.log(err);
+           
+        } else {
+                const newUser = new User({
+                    uid: req.body.uid,
+                    username: req.body.username,
+                    email:req.body.email
+                });
+                 newUser.save()
+                res.send("File Uploaded")
+            
+        }
+    });
+});
+
+
+
+app.get("/user",async (req,resp)=>{
+    const users=await User.find()
+    resp.send(users)
+})
+
+
+
+
+app.listen(4000)
