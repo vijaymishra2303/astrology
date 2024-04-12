13933530f9c0740b4d47523ac7a0c28e1dcb656a
vijaymishra2303 <vijaymishra2303@gmail.com>
2024-04-12 15:25:37 +0530

    new

--- index.js
+++ index.js
@@ -5,19 +5,17 @@ const express = require("express");
 const app = express();
 const multer = require("multer");
 const cors = require("cors");
-
- 
-
 app.use(cors());
 app.use(express.json());
+
 app.use(express.static("public"))
 
 // Multer configuration for file upload
 const storage = multer.diskStorage({
-    destination: (req, res, cb) => {
+    destination:(req, res, cb) => {
         cb(null, "public/uploads/");
     },
-    filename: (req, file, cb) => {
+    filename:(req, file, cb) => {
         cb(null, file.originalname);
     }
 });
